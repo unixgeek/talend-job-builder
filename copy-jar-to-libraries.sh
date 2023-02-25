31d4ed21929e6e01edf86992da5af0fafa5179ff
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2023-02-24 20:15:14 -0600

    Allow for 6.0.0 and 6.0.0-SNAPSHOT versions when copying to org.talend.libraries.

--- copy-jar-to-libraries.sh
+++ copy-jar-to-libraries.sh
@@ -2,8 +2,8 @@
 
 SELF=$(basename "${0}")
 
-if [ $# -ne 4 ]; then
-  echo "usage: ${SELF} TALEND_HOME GROUP_ID ARTIFACT_ID VERSION"
+if [ $# -ne 5 ]; then
+  echo "usage: ${SELF} TALEND_HOME GROUP_ID ARTIFACT_ID VERSION LIBRARY_VERSION"
   exit 1
 fi
 
@@ -11,6 +11,9 @@ TALEND_HOME="${1}"
 GROUP_ID="${2}"
 ARTIFACT_ID="${3}"
 VERSION="${4}"
+# 6.0.0 for a dependency in the org.talend.libraries without the .jar.
+# 6.0.0-SNAPSHOT for a dependency that does not exist in org.talend.libraries and is loaded with tLibraryLoad.
+LIBRARY_VERSION="${5}"
 
 # May not work universally. Based on observation.
 # From: org.mnode.ical4j ical4j 3.2.9
@@ -23,7 +26,7 @@ mvn --batch-mode --quiet org.apache.maven.plugins:maven-install-plugin:3.1.0:ins
     -Dfile="${JAR_FILE}" \
     -DgroupId=org.talend.libraries \
     -DartifactId="${ARTIFACT_ID}" \
-    -Dversion=6.0.0-SNAPSHOT \
+    -Dversion="${LIBRARY_VERSION}" \
     -Dpackaging=jar \
     -DlocalRepositoryPath="${TALEND_HOME}"/configuration/.m2/repository
 
