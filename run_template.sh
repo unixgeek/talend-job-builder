5053f390ab20f1ad6ac34d7f1c56281b053a8bdd
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2023-02-27 09:14:09 -0500

    Oops.

--- run_template.sh
+++ run_template.sh
@@ -6,7 +6,7 @@ fi
 
 cd JOB_NAME
 
-if grep -q exec JOB_NAME_run.sh; then
+if ! grep -q exec JOB_NAME_run.sh; then
   echo "WARN: shell script should have \"exec java ...\" for proper signal handling"
   echo "See https://docs.docker.com/engine/reference/builder/#exec-form-entrypoint-example"
 fi
