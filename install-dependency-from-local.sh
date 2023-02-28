b54b925a987cf31e7276d76d86f1d26506e74a72
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2023-02-28 16:11:21 -0500

    Added script to install local jar. Renamed other scripts for clarity.

---
+++ install-dependency-from-local.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+
+SELF=$(basename "${0}")
+
+if [ $# -ne 5 ]; then
+  echo "usage: ${SELF} TALEND_HOME ARTIFACT_ID LIBRARY_VERSION JAR_FILE"
+  exit 1
+fi
+
+TALEND_HOME="${1}"
+ARTIFACT_ID="${2}"
+# 6.0.0 for a dependency in the org.talend.libraries without the .jar.
+# 6.0.0-SNAPSHOT for a dependency that does not exist in org.talend.libraries and is loaded with tLibraryLoad.
+LIBRARY_VERSION="${3}"
+JAR_FILE="${4}"
+
+echo "${SELF}: Installing ${JAR_FILE}..."
+mvn --batch-mode --quiet org.apache.maven.plugins:maven-install-plugin:3.1.0:install-file \
+    -Dfile="${JAR_FILE}" \
+    -DgroupId=org.talend.libraries \
+    -DartifactId="${ARTIFACT_ID}" \
+    -Dversion="${LIBRARY_VERSION}" \
+    -Dpackaging=jar \
+    -DlocalRepositoryPath="${TALEND_HOME}"/configuration/.m2/repository
+
+echo "${SELF}: tLibraryLoad: Find by name \"${ARTIFACT_ID}.jar\""
